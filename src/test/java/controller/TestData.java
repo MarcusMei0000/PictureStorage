716ce57e7c7879e5e3d251e82ab9a6bf8dddef6f
MarcusMei000 <poliakova_polya@mail.ru>
2023-06-02 01:25:36 +0300

    Add tests for PictureService (2 tests are failed)

--- src/test/java/controller/TestData.java
+++ src/test/java/controller/TestData.java
@@ -6,11 +6,8 @@ import entity.Picture;
 public class TestData {
     public static Artist ARTIST_INVALID = new Artist(-1, "");
     public static Artist ARTIST_VALID = new Artist(1, "Шишкин");
-    public static Artist ARTIST_WITH_APOSTROPHE = new Artist(2, "L'artist");
 
-    public static Picture MORNING = new Picture(1,"Утро в сосновом лесу");
-    public static Picture EVERGREEN = new Picture(2,"Одинокая сосна");
-    public static Picture FOREST = new Picture(3,"Берёзовая роща");
 
-    public static Picture WHEAT = new Picture(4,"Рожь");
+    public static Picture PICTURE_INVALID = new Picture(-1, "");
+    public static Picture PICTURE_VALID = new Picture(1, "Утро в сосновом лесу");
 }
--- src/test/java/controller/logic/PictureServiceTests.java
+++ src/test/java/controller/logic/PictureServiceTests.java
@@ -1,9 +1,75 @@
 package controller.logic;
 
+import controller.TestData;
+import exception.InvalidNameException;
+import exception.NotFoundException;
+import org.junit.jupiter.api.Test;
 import repository.ConnectionFactoryByManager;
 import service.PictureService;
 
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class PictureServiceTests {
     private final PictureService pictureService = new PictureService(new ConnectionFactoryByManager());
+    @Test
+    public void getByExistIdTest() {
+        try {
+            var expected = TestData.PICTURE_VALID;
+            var actual = pictureService.getById(TestData.PICTURE_VALID.getIdPicture());
+            assertEquals(expected, actual);
+        } catch (SQLException e) {
+            fail(e);
+        }
+    }
+
+    @Test
+    public void getByNotExistIdTest() {
+        try {
+            long id = TestData.PICTURE_INVALID.getIdPicture();
+            var actual = pictureService.getById(id);
+            assertNull(actual);
+        } catch (SQLException e) {
+            fail(e);
+        }
+    }
+
+    @Test
+    public void createTest() {
+        try {
+            var expected = TestData.PICTURE_VALID;
+            long id = pictureService.add(expected);
+            var actual = pictureService.getById(id);
+            repository.Utils.checkEqualsNames(expected, actual);
+        } catch (SQLException | InvalidNameException e) {
+            fail(e);
+        }
+    }
+
+    @Test
+    public void saveInvalidNameTest() {
+        try {
+            var expected = TestData.PICTURE_INVALID;
+            pictureService.add(expected);
+        } catch (InvalidNameException e) {
+            fail("Save is correct, but name is empty");
+        } catch (SQLException e) {
+            fail(e);
+        }
+    }
+
+    @Test
+    public void deleteNotExists() {
+        try {
+            long id = TestData.PICTURE_INVALID.getIdPicture();
+            pictureService.delete(id);
+        } catch (NotFoundException e) {
+            fail("delete is correct, but this entity not exists");
+        } catch (SQLException e) {
+            fail(e);
+        }
+    }
 
 }
